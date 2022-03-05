2f1ae7d82d345c7cba41813f9155ea5f971c8e6b
SeeDream <64675266+buxiuzhixing@users.noreply.github.com>
2022-03-05 13:52:01 +0800

    Main file
    kfclub

---
+++ kf.py
@@ -0,0 +1,18 @@
+a=input("你是蝌粉吗？")
+if a=="是":
+    print("暗号正确访问允许！\n欢迎来到蝌粉俱乐部！\n请输入用户名与密码,账号不存在将自动创建")
+    y=input("用户名：")
+    p=input("密码：")
+    g=float(input("您的年龄？"))
+    m=input("验证码：114514：")
+    if m=="114514":
+        print("蝌粉,欢迎登录\n================\n您现在在大厅\n================\na=进入_蝌蚪SeaDream_的放映厅\nb=注销")
+        menu=input("您现在想做什么？")
+        if menu=="a":                                           print("放映厅正在播放_蝌蚪SeaDream_的>《论史蒂夫的重量》快去看看吧！https://b23.tv/gZLM6es\na=返回大厅\nb=注销\nc=点赞\nd=投币\ne=收藏\nf=打赏")
+              menu=input("您现在想做什么？")
+              if menu=
+        if menu=="b":                                           print("您已离开蝌粉俱乐部！")
+    else:
+        print("验证码错误，系统退出")
+else:
+    print("不是蝌粉请滚！")
